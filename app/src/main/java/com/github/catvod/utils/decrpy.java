2ade32c4426e82c35545eb0ee98a5912624871e8
bgcode <bglhcode@qq.com>
2025-03-16 14:15:13 +0800

    update

---
+++ app/src/main/java/com/github/catvod/utils/decrpy.java
@@ -0,0 +1,51 @@
+package com.github.bgcode.util;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class decrpy {
+    public static String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes());
+            StringBuilder hashStringBuilder = new StringBuilder();
+            for (byte b : digest) {
+                hashStringBuilder.append(String.format("%02x", b & 0xff));
+            }
+            return hashStringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // SHA-1 加密方法
+    public static String sha1(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] digest = md.digest(input.getBytes());
+            StringBuilder hashStringBuilder = new StringBuilder();
+            for (byte b : digest) {
+                hashStringBuilder.append(String.format("%02x", b & 0xff));
+            }
+            return hashStringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // SHA-256 加密方法
+    public static String sha256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(input.getBytes());
+            StringBuilder hashStringBuilder = new StringBuilder();
+            for (byte b : digest) {
+                hashStringBuilder.append(String.format("%02x", b & 0xff));
+            }
+            return hashStringBuilder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
